043b6937f9a89b6ed15ae2847c035ae3c35b9f9a
diegoram <diego@diegoram.me>
2017-04-20 10:29:12 -0300

    added ipython notebook and firts tries

---
+++ crank.py
@@ -0,0 +1,15 @@
+#Python 3.5 script
+expected = 'e61754cfd4ececbb689d83caa28c472e'
+
+'''
+Positional params array
+#1 Phone
+#2
+'''
+params = ['1154697128','1479','f0e514','neptuneads', '1083974']
+
+def run():
+    
+
+if '__name__' == '__main__':
+    run()
